87974510364f36a85fb8645ed418435e5ede11d9
killioin007-source <killioin.007@gmail.com>
2026-05-08 09:07:39 -0400

    initial commit

---
+++ src/main/java/Dealership.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    public Dealership(String name, String address, String phone, ArrayList<Vehicle> vehicles) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+
+    }
+}
