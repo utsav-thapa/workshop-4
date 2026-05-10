193ef579e6decb6b7385550dcf274c0f8904fff3
killioin007-source <killioin.007@gmail.com>
2026-05-10 12:36:08 -0400

    added some code in DealershipFileManager class

--- src/main/java/Dealership.java
+++ src/main/java/Dealership.java
@@ -17,53 +17,8 @@ public class Dealership {
     public void addVehicle(Vehicle vehicle){
 
     }
-    public static ArrayList<Vehicle> vehicles(String FilePath) {
-        ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
-        FileReader fileReader;
-        BufferedReader bufferedReader;
-
-        try {
-            fileReader = new FileReader("src/main/resources/inventory.csv");
-            bufferedReader = new BufferedReader(fileReader);
-
-            String line = bufferedReader.readLine();
-            String[] header = line.split("\\|");
-            String dealerName = header[0];
-            String dealerAddress = header[1];
-            String dealerPhone = header[2];
-            System.out.printf("%-10s %-10s %-12s","Dealer","Address","Phone Number");
-            System.out.printf("%-10s %-10s %-12s",dealerName,dealerAddress,dealerPhone);
-
-            while (line != null) {
-                String[] eachVehicle = line.split("\\|");
-                int vin = Integer.parseInt(eachVehicle[0]);
-                int year = Integer.parseInt(eachVehicle[1]);
-                String make = eachVehicle[2];
-                String model = eachVehicle[3];
-                String vehicleType = eachVehicle[4];
-                String color = eachVehicle[5];
-                int odometer = Integer.parseInt(eachVehicle[6]);
-                double price = Double.parseDouble(eachVehicle[7]);
-
-                Vehicle vehicle = new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
-            }
 
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return null;
-
-    }
-
-    public ArrayList<Vehicle> getAllVehicles(){
-//        System.out.printf("%-10s %-5s %-10s %-8s %-12s %-8s %-12s %-12s\n","Vin","Year","Make","Model","Vehicle Type","Color","Odometer","Price");
-//
-//        for(Vehicle s: inventory) {
-//            s.allVehicles();
-//        }
-        return inventory;
-    }
 
 
 }
